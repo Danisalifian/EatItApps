1750e29ad020b689cebec6d03952b1d720587db6
Danis_Alifian <danisalifian@gmail.com>
2017-12-18 23:12:33 +0700

    V3
    Sign In,Sign Up, User Panel & Menu Page, List of food

---
+++ app/src/main/java/com/dev/dan/eatitapps/Model/Food.java
@@ -0,0 +1,8 @@
+package com.dev.dan.eatitapps.Model;
+
+/**
+ * Created by DAN on 18/12/2017.
+ */
+
+public class Food {
+}
---
+++ app/src/main/java/com/dev/dan/eatitapps/ViewHolder/FoodViewHolder.java
@@ -0,0 +1,8 @@
+package com.dev.dan.eatitapps.ViewHolder;
+
+/**
+ * Created by DAN on 18/12/2017.
+ */
+
+public class FoodViewHolder {
+}
